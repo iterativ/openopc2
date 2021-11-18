cca8adbad27c24f763c0d5c7b1c29d6d0dd59222
Lorenz Padberg <lorenz.padberg@iterativ.ch>
2021-11-18 09:51:50 +0100

    improved testing for properties

--- OpenOpc/tests/test_write.py
+++ OpenOpc/tests/test_write.py
@@ -1,5 +1,6 @@
 import numbers
 from unittest import TestCase
+from colorama import Fore
 
 from OpenOpc.tests.opc_server_config import connect_opc_client
 
@@ -9,26 +10,21 @@ class TestWriteTags(TestCase):
         self.opc_client = connect_opc_client()
         self.tags = self.opc_client.list(recursive=False, include_type=False, flat=True)
         self.no_system_tags = [tag for tag in self.tags if "@" not in tag]
+        self.writeable_tags = [tag for tag in self.tags if "Bucket Brigade" in tag]
 
     def test_write_all_tags_single_writes(self):
         """
         Not really a test but if it runs through  most datatypes work
         """
-        for tag in self.tags:
+        for tag in self.writeable_tags:
             old_value = self.opc_client.read(tag)[0]
-            if old_value:
-                if isinstance(old_value, numbers.Number):
-                    new_value = old_value + 1
-                if isinstance(old_value, str):
-                    new_value = "OPC Test"
-                if isinstance(old_value, tuple):
-                    new_value = range(len(old_value))
+            if old_value is not None:
+                new_value = create_new_value(old_value)
                 write = self.opc_client.write((tag, new_value))
                 written_value = self.opc_client.read(tag)[0]
-                if new_value == written_value:
-                    print(f"Write Success: {tag:20} old: {old_value} new: {written_value} ")
-                else:
-                    print(f"Write Failed : {tag:20} old: {old_value} new: {written_value} ")
+                print_write_result(write, tag, old_value, written_value)
+            else:
+                print(old_value)
 
                 # self.assertEqual(new_value, writen_value)
                 # write = self.opc_client.write((tag, old_value))
@@ -37,22 +33,13 @@ class TestWriteTags(TestCase):
         """
         Not really a test but if it runs through  most datatypes work
         """
-        for tag in self.tags:
+        for tag in self.writeable_tags:
             old_value = self.opc_client.read(tag)[0]
-            if old_value:
-                if isinstance(old_value, numbers.Number):
-                    new_value = old_value + 1
-                if isinstance(old_value, str):
-                    new_value = "OPC Test"
-                if isinstance(old_value, tuple):
-                    new_value = range(len(old_value))
-                write = self.opc_client.write((tag, new_value), include_error=True)
-                print(write)
-                written_value = self.opc_client.read(tag)[0]
-                if new_value == written_value:
-                    print(f"Write Success: {tag:20} old: {old_value} new: {written_value} ")
-                else:
-                    print(f"Write Failed : {tag:20} old: {old_value} new: {written_value} ")
+            new_value = create_new_value(old_value)
+            write = self.opc_client.write((tag, new_value), include_error=True)
+            written_value = self.opc_client.read(tag)[0]
+            print_write_result(write, tag, old_value, written_value)
+
 
                 # self.assertEqual(new_value, writen_value)
                 # write = self.opc_client.write((tag, old_value))
@@ -60,3 +47,24 @@ class TestWriteTags(TestCase):
     def test_write_tags_list(self):
         values = self.opc_client.write(list(zip([self.no_system_tags[4], self.no_system_tags[5]], [1, 10])))
         print(f"{values}")
+
+
+def print_write_result(write_result, tag, old, new):
+    if type(write_result) == list:
+        write_result = write_result[0]
+    success = write_result[0] == 'Success'
+    color = Fore.GREEN if success else Fore.RED
+    if success:
+        print(f"{color}{write_result[0]}: {tag:20} old: {old} new: {new}")
+    else:
+        print(f"{color}{write_result}")
+
+
+
+def create_new_value(old_value):
+    if isinstance(old_value, numbers.Number):
+        return old_value + 1
+    if isinstance(old_value, str):
+        return "OPC Test"
+    if isinstance(old_value, tuple):
+        return range(len(old_value))
---
+++ doc/Roadmap.md
@@ -0,0 +1,30 @@
+Version 1.5
+
+Goals:
+- Create a maintainable repository
+  - Integration tests coverage 80 %
+  - Python 3.8, 3.9
+  - Create executables for OpenOPCService and OPC cli
+  - Create pypy package
+  - remain compatible to 1.3 and 1.2
+  
+Known Issues:
+- Return Values sometimes List of tuples and sometimes tuple... causes trouble complex code
+- write(tag, include_error=True) returns list of tuples (which should be tuple)
+- too many except: pass this is a very dangerous pattern
+
+Future Goals:
+- Proper Logging
+- code  refactoring, reduce complexity increase readablility
+- replace response tuples with named tuples / dataclasses whatever is suitable 
+- improve error handling
+- introduce encrypted protocol for gateway
+- write unittests
+- consolidate repositories and forks on github
+- test on multiple python versions and platforms
+
+Far away Goals:
+- OPC UA Gateway
+- OPC AE support
+- Rest API
+
