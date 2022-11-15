9794bf93437623d7d1d6399912110e702135de34
Lorenz Padberg <lorenz.padberg@iterativ.ch>
2022-11-15 14:30:11 +0100

    Config for build in spec file

--- scripts/build_executables.ps1
+++ scripts/build_executables.ps1
@@ -1,10 +1,1 @@
-pyinstaller --onefile `
-            --clean `
-            --noconfirm `
-            --paths ./venv/Lib/site-packages `
-            --paths ./openopc2 `
-            --hidden-import=json `
-            --hidden-import=win32timezone `
-            --hidden-import=pythoncom  `
-            --name OpenOpcService `
-            ../openopc2/gateway_service.py
+pyinstaller OpenOpcService.spec --noconfirm
