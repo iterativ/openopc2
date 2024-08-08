d121c4e5fa63e9770af3e36880c61e6c286c1bf7
renzop <lorenz.padberg@iterativ.ch>
2024-08-08 16:08:00 +0200

    Feature/fix properties reading (#62)
    * Fix bug in the properties function
    
    * Fix typo

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,7 +2,7 @@ from openopc2.config import OpenOpcConfig
 
 def test_config():
     open_opc_config = OpenOpcConfig()
-    open_opc_config.OPC_SERVER = "Matrikon.OPC.Simulation"
+    open_opc_config.OPC_SERVER = "Matrikon.OPC.Simulation.1"
     open_opc_config.OPC_GATEWAY_HOST = "192.168.0.115"
     open_opc_config.OPC_CLASS = "Graybox.OPC.DAWrapper"
     open_opc_config.OPC_MODE = 'com'
